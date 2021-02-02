af086ad09409a363402157c56d346e07847171b1
Victor Cocuz <victor.cocuz@yahoo.com>
2021-02-02 19:18:32 +0000

    feat: add fragments

---
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.loginButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_onboardingFragment)
+        )
+        return binding.root
+    }
+}
